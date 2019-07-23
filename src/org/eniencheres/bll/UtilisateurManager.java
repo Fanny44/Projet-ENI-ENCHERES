f0d9d83e67b50965aea0ecc26bf4f9db21b5b52e
Stéphane Thomarat <stephane.thomarat2019@campus-eni.fr>
2019-07-23 15:20:18 +0200

    Création de la BLL UtilisateurManager

--- src/org/eniencheres/bll/UtilisateurManager.java
+++ src/org/eniencheres/bll/UtilisateurManager.java
@@ -6,28 +6,56 @@ import org.eniencheres.dal.DAOFactory;
 import org.eniencheres.dal.DAOUtilisateur;
 
 public class UtilisateurManager {
-	private static UtilisateurManager instance;
 	
+	// déclaration des variables
+	private static UtilisateurManager instance;
+	private DAOUtilisateur dao = null;
+	// Création d'une Design Pattern Singleton
 	public static UtilisateurManager getInstance() {
 		if (instance == null) {
 			instance = new UtilisateurManager();
 		}
 		return instance;
 	}
-	
+	//constructeur afin de créer une instance d'UtilisateurManager
 	private UtilisateurManager() {
+		dao = DAOFactory.getUtilisateurDAO();	// GET d'une instance de DAOUtilisateur
 	}
 	
-	public Utilisateur verifIdentite(String pseudo, String motdepasse){
-		
-		DAOUtilisateur dao = DAOFactory.getUtilisateurDAO();
-		Utilisateur utilisateur = null;
+	
+	
+	
+	
+	/** Classe verifIdentite
+	 * Vérifications concernant l'identité
+	 * @param identifiant
+	 * @param motdepasse
+	 * @return utilisateur
+	 */
+	public Utilisateur verifIdentite(String identifiant, String motdepasse){
+		Utilisateur utilisateur = null; // initialisation de la variable utilisateur
+			
 		try {
-			utilisateur = dao.selectByPseudo(pseudo);
+			//Vérifie que le mot de passe fasse bien huit caractères
+			if(isValidpassword(motdepasse))
+			
+			// Contrôle pour savoir si l'identification se fait par email	
+			if(isValidEmail(identifiant)) {
+				utilisateur = dao.selectByEmail(identifiant);
+				
+			}
+			// Contrôle pour savoir si l'identification se fait par pseudo	
+
+			else {
+				utilisateur = dao.selectByPseudo(identifiant);
+			}
+
 		} catch (DALException e) {
 			e.printStackTrace();
 		}	
+			// Vérifie que le mot de passe soit bien renseigné
 		if (utilisateur != null) {
+			// Vérifie que le mot de passe corresponde bien au mot de passe de cet utilisateur en base de donnée
 			if (motdepasse != utilisateur.getMotDePasse() ) {
 				utilisateur = null;
 			}
@@ -35,8 +63,16 @@ public class UtilisateurManager {
 		
 		return utilisateur;
 	}
-	
- 
+			// Méthode pour savoir si l'identification se fait par mail ou par pseudo à l'aide d'une expression régulière
+	 public static boolean isValidEmail( String email ) {
+	        String exReg = "^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)+$";
+	        return email.matches( exReg );
+	    }
+	 		//Méthode pour savoir si le mot de passe fait huit caractères à l'aide d'une expression régulière
+	 public static boolean isValidpassword( String motdepasse ) {
+	        String regEx = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-z^A-Z^0-9]).{8,}$";
+	        return motdepasse.matches( regEx );
+	    }
 }
 	
 	
--- src/org/eniencheres/servlets/ServletConnection.java
+++ src/org/eniencheres/servlets/ServletConnection.java
@@ -1,20 +1,21 @@
 package org.eniencheres.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eniencheres.bll.UtilisateurManager;
+import org.eniencheres.bo.Utilisateur;
+
 /**
  * Servlet implementation class ServletEniEncheres
  */
-public class ServletEniEncheres extends HttpServlet {
+public class ServletConnection extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	// Définition de deux variables identifiant et mot de passe.
-	String identifiant ;
-	String motdepasse ;
+	
     
 
 	/**
@@ -22,18 +23,26 @@ public class ServletEniEncheres extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//Affichage de la page de connexion.
-		request.getRequestDispatcher("/WEB-INF/jsp/connexion.jsp").forward(request, response);//
+		request.getRequestDispatcher("/WEB-INF/jsp/connexion.jsp").forward(request, response);
+	
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//Récupération de l'identifiant et du mot de passe tapés par l'utiliisateur sur la page de connexion et stockage dans variables identifiant et motdepasse.
-		 identifiant = request.getParameter("identifiant");
-		 motdepasse = request.getParameter("motdepasse");
-		 
-		
+		//Rï¿½cupï¿½ration de l'identifiant et du mot de passe tapï¿½s par l'utiliisateur sur la page de connexion et stockage dans variables identifiant et motdepasse.
+			String identifiant = request.getParameter("identifiant");
+			String motdepasse = request.getParameter("motdepasse");
+			UtilisateurManager um = UtilisateurManager.getInstance();
+			Utilisateur utilisateur = um.verifIdentite(identifiant, motdepasse);
+			
+			if(utilisateur == null){
+				System.out.println("Erreur");
+			}
+			
 	}
+	
+	
 
 }
