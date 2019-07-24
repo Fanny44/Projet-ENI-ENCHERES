bf7fc97c07bcab902eea3f09cb0da46c4e14f1ff
Stéphane Thomarat <stephane.thomarat2019@campus-eni.fr>
2019-07-24 17:20:29 +0200

    ServletControleDeConnexion créée. Affichera le statut de connexion.

--- src/org/eniencheres/servlets/ServletControleDeConnexion.java
+++ src/org/eniencheres/servlets/ServletControleDeConnexion.java
@@ -6,27 +6,27 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class ServletDeconnexion
+ * Servlet implementation class ServletControleDeConnexion
  */
-@WebServlet("/ServletDeconnexion")
-public class ServletDeconnexion extends HttpServlet {
+@WebServlet("/ServletControleDeConnexion")
+public class ServletControleDeConnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	    String un=request.getParameter("uname");
+        String pw=request.getParameter("pass");
+
+        HttpSession session= request.getSession();
+        session.setAttribute("name", un);
+
+        response.sendRedirect("home.jsp");
+				
 	}
 
 }
