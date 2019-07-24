b40d3ad4e9245aa3f5db05da41872deada8970e8
Christophe Michard <christophe.michard2019@campus-eni.fr>
2019-07-24 14:35:56 +0200

    Modification du contrat url

--- src/org/eniencheres/bo/ContratUrl.java
+++ src/org/eniencheres/bo/ContratUrl.java
@@ -2,10 +2,9 @@ package org.eniencheres.bo;
 
 public class ContratUrl {
 
-	public static final String urlFrgBaliseHead = "/WEB-INF/fragments/baliseHead.jsp";
+	public static final String URL_FRG_BALISE_HEAD = "/WEB-INF/fragments/baliseHead.jsp";
 
-	public static final String URL_Inscription = "/WEB-INF/jsp/inscription.jsp";
-	
+	public static final String URL_INSCRIPTION = "/WEB-INF/jsp/inscription.jsp";
 	public static final String URL_ACCUEIL = "/WEB-INF/jsp/Accueil.jsp"; 
 	
 }
